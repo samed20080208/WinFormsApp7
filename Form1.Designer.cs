b38ae7ca75668a371cee3fedbf4d6b01d9307604
samedyolchuyev@outlook.com <samedyolchuyev@outlook.com>
2024-04-12 23:49:43 +0400

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace WinFormsApp7
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            maskedTextBox1 = new MaskedTextBox();
+            maskedTextBox2 = new MaskedTextBox();
+            button1 = new Button();
+            button2 = new Button();
+            listBox1 = new ListBox();
+            textBox4 = new TextBox();
+            button3 = new Button();
+            button4 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(64, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(22, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Ad";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(64, 60);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Soyad";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(64, 90);
+            label3.Name = "label3";
+            label3.Size = new Size(36, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Email";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(108, 25);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(149, 23);
+            textBox1.TabIndex = 3;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(108, 57);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(149, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(108, 87);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(149, 23);
+            textBox3.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(64, 122);
+            label4.Name = "label4";
+            label4.Size = new Size(41, 15);
+            label4.TabIndex = 6;
+            label4.Text = "Phone";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(64, 151);
+            label5.Name = "label5";
+            label5.Size = new Size(34, 15);
+            label5.TabIndex = 8;
+            label5.Text = "Date ";
+            // 
+            // maskedTextBox1
+            // 
+            maskedTextBox1.Location = new Point(108, 119);
+            maskedTextBox1.Mask = "(999) 000-0000";
+            maskedTextBox1.Name = "maskedTextBox1";
+            maskedTextBox1.Size = new Size(149, 23);
+            maskedTextBox1.TabIndex = 9;
+            // 
+            // maskedTextBox2
+            // 
+            maskedTextBox2.Location = new Point(108, 148);
+            maskedTextBox2.Mask = "00/00/0000";
+            maskedTextBox2.Name = "maskedTextBox2";
+            maskedTextBox2.Size = new Size(149, 23);
+            maskedTextBox2.TabIndex = 10;
+            maskedTextBox2.ValidatingType = typeof(DateTime);
+            // 
+            // button1
+            // 
+            button1.Location = new Point(64, 177);
+            button1.Name = "button1";
+            button1.Size = new Size(193, 29);
+            button1.TabIndex = 11;
+            button1.Text = "Change";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(64, 212);
+            button2.Name = "button2";
+            button2.Size = new Size(193, 29);
+            button2.TabIndex = 12;
+            button2.Text = "Add";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(263, 27);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(163, 154);
+            listBox1.TabIndex = 13;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(263, 187);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(163, 23);
+            textBox4.TabIndex = 14;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(263, 216);
+            button3.Name = "button3";
+            button3.Size = new Size(80, 25);
+            button3.TabIndex = 15;
+            button3.Text = "Save";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(346, 216);
+            button4.Name = "button4";
+            button4.Size = new Size(80, 25);
+            button4.TabIndex = 16;
+            button4.Text = "Load";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(textBox4);
+            Controls.Add(listBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(maskedTextBox2);
+            Controls.Add(maskedTextBox1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Label label4;
+        private Label label5;
+        private MaskedTextBox maskedTextBox1;
+        private MaskedTextBox maskedTextBox2;
+        private Button button1;
+        private Button button2;
+        private ListBox listBox1;
+        private TextBox textBox4;
+        private Button button3;
+        private Button button4;
+    }
+}
